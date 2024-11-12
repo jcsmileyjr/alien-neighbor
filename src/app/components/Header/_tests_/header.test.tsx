939f60d1e66654e39b1f3912e6416b3c50b48dab
JC <jcsmileyjr@gmail.com>
2024-11-12 15:24:01 -0600

    Test Header: Test that the app logo is in the header

--- src/app/components/Header/_tests_/header.test.tsx
+++ src/app/components/Header/_tests_/header.test.tsx
@@ -6,4 +6,10 @@ describe('Header', () => {
         render(<Header />);
         expect(screen.getByText('Space Colony Sign Up')).toBeInTheDocument();
     });
+
+    it('should render the Alien Neighbor logo', () => {
+        render(<Header />);
+        const img = screen.getByRole('img')
+        expect(img).toHaveAttribute('src', "/_next/image?url=%2Fimages%2Falien-planet.jpg&w=64&q=75")
+    });
 });
