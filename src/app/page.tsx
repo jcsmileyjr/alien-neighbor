589b0f3f31919eae8f8e92046eb47a5c5aecc13e
JC <jcsmileyjr@gmail.com>
2024-11-12 15:13:48 -0600

    Feat Footer: Add content and style to the Footer

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,13 +3,13 @@ import Header from "./components/Header/Header";
 
 export default function Home() {
   return (
-    <div className="py-4">
+    <div className="py-4 h-screen flex flex-col justify-between">
       <Header />
       <main className="">
         Hello World
       </main>
-      <footer className="">
-
+      <footer className="text-center">
+        By JC Smiley
       </footer>
     </div>
   );
