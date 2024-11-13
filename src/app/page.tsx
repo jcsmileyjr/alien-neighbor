1537ad3746b118727b52e2ddd6f42b48d5e7e0ca
JC <jcsmileyjr@gmail.com>
2024-11-13 12:26:13 -0600

    Feat: Style and build the intro UI section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,11 +3,17 @@ import { setName } from "@/redux/features/userInformationSlice";
 
 export default function Home() {
   return (
-    <div className="p-4 h-screen flex flex-col justify-between sm:mx-auto sm:w-96">
-      <Header />
-      <main className="">
-        Hello World
-      </main>
+    <div className="p-4 h-screen flex flex-col justify-between sm:mx-auto md:w-2/3 lg:w-1/2">
+      <div className="flex flex-col gap-4">
+        <Header />
+        <main className="">
+          <section className="text-center text-base">
+            <h2 className="font-extrabold text-xl">We Live Everywhere, even <span className="whitespace-nowrap">Dark Places</span></h2>
+            <p className="font-bold">Visit the stars and touch aliens</p>
+            <p className="">Join the Colony Settlers</p>
+          </section>
+        </main>
+      </div>
       <footer className="text-center">
         By JC Smiley
       </footer>
