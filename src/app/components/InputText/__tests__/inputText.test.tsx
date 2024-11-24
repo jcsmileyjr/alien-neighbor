b4eab6701c9577551563a76c90ae791a3abfbda8
JC <jcsmileyjr@gmail.com>
2024-11-23 21:40:21 -0600

    Refactor Input Text: Remove un-used variable

--- src/app/components/InputText/__tests__/inputText.test.tsx
+++ src/app/components/InputText/__tests__/inputText.test.tsx
@@ -1,7 +1,7 @@
 import {render, screen} from '@testing-library/react';
 import InputText from '../InputText';
 import { User } from "@/User";
-import { useForm, FieldError } from "react-hook-form"
+import { useForm} from "react-hook-form"
 const {register, formState: { errors }} = useForm<User>();
 
 describe('InputText', () => {
