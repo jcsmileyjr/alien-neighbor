f3ed76787506c19610b19631fb9b4751a550aa65
JC <jcsmileyjr@gmail.com>
2024-11-23 21:07:36 -0600

    Build: Testing a solution

--- src/app/components/InputText/InputText.tsx
+++ src/app/components/InputText/InputText.tsx
@@ -1,10 +1,10 @@
-import { UseFormRegister, FieldError } from "react-hook-form";
+import { UseFormRegister, FieldError} from "react-hook-form";
 import { User } from "@/User";
 
-const InputText = ({ name, label, notes, errors, register }: { name: "user_name" | "user_illegal_name", label: string, notes?: string, errors: FieldError | undefined, register: UseFormRegister<User> }) => {
+const InputText = ({ name, label, notes, errors, register }: { name: "user_name" | "user_illegal_name", label: string, notes?: string, errors?: FieldError | undefined, register: UseFormRegister<User> }) => {
     return (
         <div className="mb-4">
-            <label className="text-base font-bold flex flex-row justify-between w-full" htmlFor={name}>{label} {errors && errors[name as keyof typeof errors] && <p className="text-sm text-red-500">Required</p>}</label>
+            <label className="text-base font-bold flex flex-row justify-between w-full" htmlFor={name}>{label} {errors && errors[name as keyof typeof errors] as FieldError && <p className="text-sm text-red-500">Required</p>}</label>
             <input {...register(name, { required: true })} id={name} type="text" placeholder="Type here" className="border border-blueBlack border-2 p-2 rounded w-full " />
             {notes && <p className="text-base text-offBlack">{notes}</p>}
             
