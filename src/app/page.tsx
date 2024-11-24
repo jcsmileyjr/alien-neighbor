1cd310d4bc6a5244bd475742d7a093c698074dd4
JC <jcsmileyjr@gmail.com>
2024-11-24 07:53:53 -0600

    Refactor & Style Header: Tweak to reduce clutter

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,8 +8,7 @@ export default function Home() {
         <Header />
         <main className="flex flex-col gap-4">
           <section className="text-center text-base mb-4">
-            <h2 className="font-extrabold text-xl">We Live Everywhere, even <span className="whitespace-nowrap">Dark Places</span></h2>
-            <p className="font-bold">Visit the stars and touch aliens</p>
+            <h2 className="font-extrabold text-xl">Visit the stars and touch aliens</h2>
             <p className="">Join the Colony Settlers</p>
           </section>
           <SignUpForm />          
