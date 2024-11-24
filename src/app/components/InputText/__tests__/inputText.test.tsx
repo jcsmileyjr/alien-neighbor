359b69afdbd4b65b511d8e0f96e258097ac843b0
JC <jcsmileyjr@gmail.com>
2024-11-23 21:44:18 -0600

    Build issues in the test cases
    The issues was that the useForm was being called outside of the test suites

--- src/app/components/InputText/__tests__/inputText.test.tsx
+++ src/app/components/InputText/__tests__/inputText.test.tsx
@@ -2,15 +2,17 @@ import {render, screen} from '@testing-library/react';
 import InputText from '../InputText';
 import { User } from "@/User";
 import { useForm} from "react-hook-form"
-const {register, formState: { errors }} = useForm<User>();
+
 
 describe('InputText', () => {
     it('should render', () => {
+        const {register, formState: { errors }} = useForm<User>();
         render(<InputText errors={errors?.user_name } register={register} name="user_name" label="Name" />);
         expect(screen.getByLabelText('Name')).toBeInTheDocument();
     });
 
-    it('should render notes', () => {        
+    it('should render notes', () => {  
+        const {register, formState: { errors }} = useForm<User>();      
         render(<InputText errors={errors?.user_name } register={register} name="user_name" label="Name" notes="Notes"/>);
         expect(screen.getByText('Notes')).toBeInTheDocument();
     });
--- src/app/components/SelectInput/__tests__/selectInput.test.tsx
+++ src/app/components/SelectInput/__tests__/selectInput.test.tsx
@@ -2,10 +2,10 @@ import {render, screen} from '@testing-library/react';
 import SelectInput from '../SelectInput';
 import { User } from "@/User";
 import { useForm} from "react-hook-form"
-const {register} = useForm<User>();
 import commute from "@/app/lib/commute";
 
 describe('SelectInput', () => {
+    const {register} = useForm<User>();
     const labelTitle = 'How far do you mind commuting to work';
     it('should render', () => {
         render(<SelectInput options={commute} name="user_commute_distance" label={labelTitle} register={register} />);
