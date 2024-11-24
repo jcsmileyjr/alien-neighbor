6b18464eb4fbefaaf8aa1d74846685b5035f43cb
JC <jcsmileyjr@gmail.com>
2024-11-24 06:43:20 -0600

    Test Select Input: solve usage of react-hook-form
    Needed to wrap the useForm first in a react component wrapper.

--- src/app/components/SelectInput/__tests__/selectInput.test.tsx
+++ src/app/components/SelectInput/__tests__/selectInput.test.tsx
@@ -1,14 +1,22 @@
-import {render, screen} from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 import SelectInput from '../SelectInput';
 import { User } from "@/User";
-import { useForm} from "react-hook-form"
+import { useForm } from "react-hook-form";
 import commute from "@/app/lib/commute";
 
+// Create a wrapper component to handle `useForm`
+const SelectInputWrapper = (props: any) => {
+    const { register } = useForm<User>();
+    const labelTitle = 'How far do you mind commuting to work';
+    return <SelectInput options={commute} name="user_commute_distance" label={labelTitle} register={register} />;
+};
+
 describe('SelectInput', () => {
-    const {register} = useForm<User>();
     const labelTitle = 'How far do you mind commuting to work';
     it('should render', () => {
-        render(<SelectInput options={commute} name="user_commute_distance" label={labelTitle} register={register} />);
+        render(
+            <SelectInputWrapper/>
+        );
         expect(screen.getByLabelText(labelTitle)).toBeInTheDocument();
     });
 });
