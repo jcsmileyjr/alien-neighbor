29b785501717d90267ff3cc1868b57555cf73c78
JC <jcsmileyjr@gmail.com>
2024-11-23 21:38:04 -0600

    Test Input Text: Fix the build error

--- src/app/components/InputText/__tests__/inputText.test.tsx
+++ src/app/components/InputText/__tests__/inputText.test.tsx
@@ -1,17 +1,17 @@
 import {render, screen} from '@testing-library/react';
 import InputText from '../InputText';
 import { User } from "@/User";
-import { useForm} from "react-hook-form"
-const {register} = useForm<User>();
+import { useForm, FieldError } from "react-hook-form"
+const {register, formState: { errors }} = useForm<User>();
 
 describe('InputText', () => {
     it('should render', () => {
-        render(<InputText register={register} name="user_name" label="Name" />);
+        render(<InputText errors={errors?.user_name } register={register} name="user_name" label="Name" />);
         expect(screen.getByLabelText('Name')).toBeInTheDocument();
     });
 
     it('should render notes', () => {        
-        render(<InputText register={register} name="user_name" label="Name" notes="Notes"/>);
+        render(<InputText errors={errors?.user_name } register={register} name="user_name" label="Name" notes="Notes"/>);
         expect(screen.getByText('Notes')).toBeInTheDocument();
     });
 });
--- src/app/components/SelectInput/__tests__/selectInput.test.tsx
+++ src/app/components/SelectInput/__tests__/selectInput.test.tsx
@@ -5,7 +5,7 @@ import { useForm} from "react-hook-form"
 const {register} = useForm<User>();
 import commute from "@/app/lib/commute";
 
-describe('SearchInput', () => {
+describe('SelectInput', () => {
     const labelTitle = 'How far do you mind commuting to work';
     it('should render', () => {
         render(<SelectInput options={commute} name="user_commute_distance" label={labelTitle} register={register} />);
