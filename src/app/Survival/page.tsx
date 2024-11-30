92e2b294ec44aa6b926fc85a86321d4f030ab05a
JC <jcsmileyjr@gmail.com>
2024-11-29 18:38:02 -0600

    Style Survive: Tweak the number spiders

--- src/app/Survival/page.tsx
+++ src/app/Survival/page.tsx
@@ -50,22 +50,30 @@ export default function Survival() {
                         </figure>
                         <div className="flex flex-col gap-4">
                             <div className="flex flex-row flex-wrap gap-2">
-                                <Image src="/images/orange-eye-spider.png" alt="hungry spider" className="flex-1 transform animate-fadeOut2" width={75} height={75} />
-                                <Image src="/images/orange-eye-spider.png" alt="hungry spider" className="flex-1 transform animate-fadeOut25" width={75} height={75} />
-                                <Image src="/images/orange-eye-spider.png" alt="hungry spider" className="flex-1 transform animate-fadeOut3" width={75} height={75} />
-                                <button onClick={handleClick} type="button" className="flex-1 bg-spaceGold border border-blueBlack border-2 text-blueBlack p-1 rounded transform animate-fadeOut4">They are Coming</button>
-                                <Image src="/images/orange-eye-spider.png" alt="hungry spider" className="flex-1 transform animate-fadeOut4" width={75} height={75} />
-                                <button onClick={handleClick} type="button" className="flex-1 bg-spaceGold border border-blueBlack border-2 text-blueBlack p-1 rounded animate-fadeOut2">A Small Taste</button>
-                                <Image src="/images/orange-eye-spider.png" alt="hungry spider" className="flex-1 transform animate-fadeOut25" width={75} height={75} />
-                                <Image src="/images/orange-eye-spider.png" alt="hungry spider" className="flex-1 animate-fadeOut3" width={75} height={75} />
-                                <button onClick={handleClick} type="button" className="flex-1 bg-spaceGold border border-blueBlack border-2 text-blueBlack p-1 rounded animate-fadeOut2">Run</button>
-                                <Image src="/images/orange-eye-spider.png" alt="hungry spider" className="flex-1 transform animate-fadeOut4" width={75} height={75} />
-                                <Image src="/images/orange-eye-spider.png" alt="hungry spider" className="flex-1 transform animate-fadeOut25" width={75} height={75} />
-                                <Image src="/images/orange-eye-spider.png" alt="hungry spider" className="flex-1 animate-fadeOut3" width={75} height={75} />
-                                <Image src="/images/orange-eye-spider.png" alt="hungry spider" className="flex-1 transform animate-fadeOut4" width={75} height={75} />
-                                <Image src="/images/orange-eye-spider.png" alt="hungry spider" className="flex-1 transform animate-fadeOut25" width={75} height={75} />
-                                <Image src="/images/orange-eye-spider.png" alt="hungry spider" className="flex-1 animate-fadeOut2" width={75} height={75} />
-                                <button onClick={handleClick} type="button" className="flex-1 bg-spaceGold border border-blueBlack border-2 text-blueBlack p-1 rounded animate-fadeOut3">Dinner</button>
+                                <Image src="/images/orange-eye-spider.png" alt="hungry spider" className="transform animate-fadeOut2" width={75} height={75} />
+                                <Image src="/images/orange-eye-spider.png" alt="hungry spider" className="transform animate-fadeOut25" width={75} height={75} />
+                                <Image src="/images/orange-eye-spider.png" alt="hungry spider" className="transform animate-fadeOut3" width={75} height={75} />
+                                <button onClick={handleClick} type="button" className="bg-blueBlack border border-blueBlack border-2 text-spaceGold  w-20 p-1 opacity-25 rounded transform animate-fadeOut4">They are Coming</button>
+                                <Image src="/images/orange-eye-spider.png" alt="hungry spider" className="transform animate-fadeOut25" width={75} height={75} />
+                                <Image src="/images/orange-eye-spider.png" alt="hungry spider" className="transform animate-fadeOut3" width={75} height={75} />
+                                <Image src="/images/orange-eye-spider.png" alt="hungry spider" className="transform animate-fadeOut4" width={75} height={75} />
+                                <button onClick={handleClick} type="button" className="bg-blueBlack border border-blueBlack border-2 text-spaceGold w-20 p-1 opacity-25 rounded animate-fadeOut2">A Small Taste</button>
+                                <Image src="/images/orange-eye-spider.png" alt="hungry spider" className="transform animate-fadeOut25" width={75} height={75} />
+                                <Image src="/images/orange-eye-spider.png" alt="hungry spider" className="transform animate-fadeOut3" width={75} height={75} />
+                                <Image src="/images/orange-eye-spider.png" alt="hungry spider" className="transform animate-fadeOut25" width={75} height={75} />
+                                <Image src="/images/orange-eye-spider.png" alt="hungry spider" className="animate-fadeOut3" width={75} height={75} />
+                                <button onClick={handleClick} type="button" className="bg-blueBlack border border-blueBlack border-2 text-spaceGold w-20 p-1 opacity-25 rounded animate-fadeOut2">Run</button>
+                                <Image src="/images/orange-eye-spider.png" alt="hungry spider" className="transform animate-fadeOut25" width={75} height={75} />
+                                <Image src="/images/orange-eye-spider.png" alt="hungry spider" className="transform animate-fadeOut3" width={75} height={75} />
+                                <Image src="/images/orange-eye-spider.png" alt="hungry spider" className="transform animate-fadeOut4" width={75} height={75} />
+                                <Image src="/images/orange-eye-spider.png" alt="hungry spider" className="transform animate-fadeOut25" width={75} height={75} />
+                                <Image src="/images/orange-eye-spider.png" alt="hungry spider" className="animate-fadeOut3" width={75} height={75} />
+                                <Image src="/images/orange-eye-spider.png" alt="hungry spider" className="transform animate-fadeOut4" width={75} height={75} />
+                                <Image src="/images/orange-eye-spider.png" alt="hungry spider" className="transform animate-fadeOut25" width={75} height={75} />
+                                <button onClick={handleClick} type="button" className="bg-blueBlack border border-blueBlack border-2 text-spaceGold w-20 p-1 opacity-25 rounded animate-fadeOut3">Dinner</button>
+                                <Image src="/images/orange-eye-spider.png" alt="hungry spider" className="transform animate-fadeOut25" width={75} height={75} />
+                                <Image src="/images/orange-eye-spider.png" alt="hungry spider" className="transform animate-fadeOut3" width={75} height={75} />
+                                <Image src="/images/orange-eye-spider.png" alt="hungry spider" className="animate-fadeOut2" width={75} height={75} />
                             </div>
 
                         </div>
